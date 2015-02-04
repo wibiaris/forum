121e0b60ea341cddc4c7d46eee26cc656871ad60
wibiaristama <wibi.aristama@gmail.com>
2015-02-04 11:20:35 +0700

    Add association between post and user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150204035542) do
+ActiveRecord::Schema.define(version: 20150204040735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150204035542) do
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
